957f479394edb050cfd52ba1cb112d97c5786b03
jaydenhs <j3hsiao@uwaterloo.ca>
2024-03-29 16:47:28 -0400

    Add key prop

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -11,8 +11,8 @@ export default function Nav() {
   return (
     <div className="fixed left-4 top-1/2 -translate-y-1/2 space-y-0">
       {sections.map((section) => (
-        <Link href={`#${section.text}`} className="block h-5">
-          <div className="group flex items-center space-x-2" id={section.id}>
+        <Link href={`#${section.text}`} className="block h-5" key={section.id}>
+          <div className="group flex items-center space-x-2">
             <div className="w-6 group-hover:w-16 h-1.5 rounded-xl bg-gray-900 transition-all duration-200" />
             <p className="opacity-0 group-hover:opacity-100 transition">
               {section.text}
