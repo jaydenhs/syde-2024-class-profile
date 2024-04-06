bfa251b09bc381b5966c23e54726f827bf78b9e9
jaydenhs <j3hsiao@uwaterloo.ca>
2024-04-06 16:23:17 -0400

    Add nmore plots

--- analysis/life.py
+++ analysis/life.py
@@ -3,11 +3,12 @@ import pandas as pd
 from source import load_data
 from generic import pie_plot
 
-
 def political_leaning(df: pd.DataFrame, show=True):
     return pie_plot(df, 'life', 'politics', show=show)
 
+def myers_briggs(df: pd.DataFrame, show=True):
+    return pie_plot(df, 'fun', 'myers-briggs', show=show)
 
 if __name__ == "__main__":
     df = load_data()
-    political_leaning(df)
+    myers_briggs(df)
--- analysis/syde.py
+++ analysis/syde.py
@@ -10,7 +10,9 @@ def restart_program(df: pd.DataFrame, **kwargs):
 def close_friends(df: pd.DataFrame, **kwargs):
     return pie_plot(df, 'syde', 'close-friends', **kwargs)
 
+def transfer_well_integrated(df: pd.DataFrame, **kwargs):
+    return pie_plot(df, 'syde', 'transfers-well-integrated', **kwargs)
+
 if __name__ == "__main__":
     df = load_data()
-    close_friends(df)
-    
+    transfer_well_integrated(df)
