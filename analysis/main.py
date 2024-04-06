8a37505b4f67055ad1e02ddfab9524d84a4f346e
Daniel Raymond <dan@raymond.ch>
2024-04-06 15:24:45 -0400

    Partial generation

--- analysis/main.py
+++ analysis/main.py
@@ -25,33 +25,39 @@ def style_figure(fig: go.Figure):
     return fig
 
 
-def generate_plots(save_dir=None, replace=False, show=False):
+def generate_plots(save_dir=None, replace=False, show=False, section=None):
     df = load_data()
     fig_dirs = {
         '1-background': {
-            'ethnicity': background.ethnicity(df, show=show),
-            'gender': background.gender(df, show=show),
-            'parent_education': background.parent_education(df, show=show),
-            'sexual_orientation': background.sexual_orientation(df, show=show),
-            # 'international': background.international(df, show=show),
+            'ethnicity': background.ethnicity(df, show=False),
+            'gender': background.gender(df, show=False),
+            'parent_education': background.parent_education(df, show=False),
+            'sexual_orientation': background.sexual_orientation(df, show=False),
+            # 'international': background.international(df, show=False),
         },
         '2-academics': {
-            'ease_vs_use': academics.ease_vs_use(df, show=show),
-            'attendance': academics.attendance(df, show=show),
-            'grades': academics.grades(df, show=show),
-            'how_challenging': academics.challenging(df, show=show),
-            'attendance_vs_grades': academics.attendance_vs_grades(df, show=show),
-            # 'friends_vs_grades': academics.friends_vs_grades(df, show=show),
+            'ease_vs_use': academics.ease_vs_use(df, show=False),
+            'attendance': academics.attendance(df, show=False),
+            'grades': academics.grades(df, show=False),
+            'how_challenging': academics.challenging(df, show=False),
+            'attendance_vs_grades': academics.attendance_vs_grades(df, show=False),
+            # 'friends_vs_grades': academics.friends_vs_grades(df, show=False),
         },
         '3-co-op': {
-            'salary': coop.salary(df, show=show),
-            'work_model': coop.work_model(df, show=show),
-            # 'salary_vs_grades': coop.grades_vs_salary(df, show=show),
+            'salary': coop.salary(df, show=False),
+            'work_model': coop.work_model(df, show=False),
+            # 'salary_vs_grades': coop.grades_vs_salary(df, show=False),
         },
         '4-syde': {
-            'restart-program': syde.restart_program(df, show=show),
+            'restart-program': syde.restart_program(df, show=False),
         },
     }
+
+    if show:
+        for subdir, figs in fig_dirs.items():
+            if section is None or section in subdir:
+                for fig in figs.values():
+                    fig.show()
 
     if save_dir is not None:
         for subdir, figs in fig_dirs.items():
@@ -69,4 +75,4 @@ def generate_plots(save_dir=None, replace=False, show=False):
 
 if __name__ == "__main__":
     # generate_plots('public/graphs/', replace=True)
-    generate_plots(show=True)
+    generate_plots(show=True, section='coop')
