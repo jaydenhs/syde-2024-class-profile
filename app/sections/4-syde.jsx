ed17e66ae509dfb1833873943999fe2fd0e5018c
jaydenhs <j3hsiao@uwaterloo.ca>
2024-04-07 21:44:17 -0400

    Add SYDE images

--- app/sections/4-syde.jsx
+++ app/sections/4-syde.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 import Section from "@components/section";
 import Graph from "@components/graph";
-import ImageBlob from "@components/image-blob";
+import ImageCollection from "@components/image-collection";
 import SpotifyEmbed from "@components/music/spotify-embed";
+
+import IMG_1087 from "@images/4-syde/IMG_1087.jpg";
+import IMG_1750 from "@images/4-syde/IMG_1750.jpg";
+import IMG_4234 from "@images/4-syde/IMG_4234.jpeg";
+import IMG_8072 from "@images/4-syde/IMG_8072.jpg";
+import IMG_9417 from "@images/4-syde/IMG_9417.jpg";
 
 export default function Syde() {
   return (
@@ -13,6 +19,7 @@ export default function Syde() {
         title="59% of us have five or more close friends in SYDE."
         srcs={["4-syde/close-friends.html"]}
       ></Graph>
+      <ImageCollection srcs={[IMG_8072, IMG_9417, IMG_4234]} />
       <Graph
         className="full"
         title="67% of us would've chosen SYDE if we could start university again."
@@ -25,14 +32,15 @@ export default function Syde() {
         percentages seen between 2018 and 2021.
       </Graph>
       <Graph
-        title="Most of us were not romantically involved with someone in SYDE."
-        srcs={["4-syde/sydecest.html"]}
-      ></Graph>
-      <Graph
-        title="56% of us have had a crush on a classmate."
-        srcs={["4-syde/crush.html"]}
+        className="full"
+        title="73% of us were not romantically involved with someone in SYDE."
+        srcs={["4-syde/sydecest.html", "4-syde/crush.html"]}
+        questions={[
+          "Have you committed SYDEcest? Any romantic involvement with someone in SYDE counts",
+          "Have you ever had a crush on someone in the cohort? If yes, have you ever confessed?",
+        ]}
       >
-        Of those, 38% have confessed their feelings.
+        However, 56% of us have had a crush on a classmate.
       </Graph>
 
       <SpotifyEmbed
