e64b0278fe71a01d299062a4049387f8ab8245b5
jaydenhs <j3hsiao@uwaterloo.ca>
2024-04-08 10:51:56 -0400

    Add key to right element

--- app/components/graph.jsx
+++ app/components/graph.jsx
@@ -18,7 +18,7 @@ export default function Graph({
 
       <div className="flex flex-col space-y-6 md:flex-row md:space-x-8 md:space-y-0">
         {srcs.map((src, index) => (
-          <div className="flex flex-col space-y-2 w-full">
+          <div key={index} className="flex flex-col space-y-2 w-full">
             {questions[index] && (
               <div className="flex items-center space-x-2 text-slate-500">
                 <Image className="opacity-50" src={HelpIcon} />
@@ -26,7 +26,6 @@ export default function Graph({
               </div>
             )}
             <iframe
-              key={index}
               className="min-h-[360px] md:min-h-[600px]"
               src={`/graphs/${src}?show_link=false`}
             />
